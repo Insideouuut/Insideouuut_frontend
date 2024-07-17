23b786e82b7d093eb69e27360f1f8ae2769d0f9a
Heesun <heesun729@naver.com>
2024-07-17 17:47:54 +0900

    [#11] ✨ 내 주소의 동까지 불러오기 구현

--- src/pages/SetLocation/MapComponent.tsx
+++ src/pages/SetLocation/MapComponent.tsx
@@ -1,17 +1,19 @@
+// MapComponent.tsx
 import React, { useEffect } from 'react';
 
-const MapComponent: React.FC = () => {
+interface MapComponentProps {
+  onLocationSelect: (location: string, lat: number, lng: number) => void; // 콜백 함수 타입 정의
+}
+
+const MapComponent: React.FC<MapComponentProps> = ({ onLocationSelect }) => {
   useEffect(() => {
     const { kakao } = window;
 
-    // 현재 위치 가져오기
     navigator.geolocation.getCurrentPosition((position) => {
       const lat = position.coords.latitude;
       const lng = position.coords.longitude;
 
-      // 현재 위치의 LatLng 객체 생성
       const locPosition = new kakao.maps.LatLng(lat, lng);
-
       const container = document.getElementById('map');
       if (!container) return;
 
@@ -20,22 +22,23 @@ const MapComponent: React.FC = () => {
         level: 3,
       };
 
-      // 지도 생성 및 현재 위치 중심 설정
       const map = new kakao.maps.Map(container, options);
-
-      // 현재 위치에 마커 표시
       const marker = new kakao.maps.Marker({
         position: locPosition,
       });
       marker.setMap(map);
-    });
-  }, []);
 
-  return (
-    <div>
-      <div id="map" style={{ width: '800px', height: '500px' }} />
-    </div>
-  );
+      const geocoder = new kakao.maps.services.Geocoder();
+      geocoder.coord2RegionCode(lng, lat, (result, status) => {
+        if (status === 'OK') {
+          const fullAddress = `${result[0].region_1depth_name} ${result[0].region_2depth_name} ${result[0].region_3depth_name}`;
+          onLocationSelect(fullAddress, lat, lng);
+        }
+      });
+    });
+  }, [onLocationSelect]);
+
+  return <div id="map" style={{ width: '800px', height: '500px' }} />;
 };
 
 export default MapComponent;
