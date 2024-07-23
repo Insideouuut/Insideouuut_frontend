2e2489cb66c1700c9092db094d9eee2f1c7bf4ca
JooHwanLeeJJang <joohwan7063@gmail.com>
2024-07-23 11:10:50 +0900

    [#37] ✨동아리메인페이지

--- src/pages/ClubPage/ex.tsx
+++ src/pages/ClubPage/ex.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+// 너비 설정 완료 수정시 말 해주삼
 const exPage: React.FC = () => {
   return (
     <div className="flex p-6 bg-gray-50 rounded-lg w-[820px] border-2 border-gray-200 "></div>
