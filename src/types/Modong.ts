bf6d238a76365e46c633211c7fd51789b74e2a5e
gangmin2 <min0115b@gmail.com>
2024-08-02 01:20:24 +0900

    [#26] ✨ 모동 관련 인터페이스 생성

---
+++ src/types/Modong.ts
@@ -0,0 +1,33 @@
+export interface Group {
+  category: string;
+  categoryDetail: string;
+  level: string;
+  hasMembershipFee: boolean;
+  membershipFeeAmount: number;
+  date: string;
+  participantLimit: number;
+  genderRatio: string;
+  ratio: string;
+  ageRange: number[];
+  name: string;
+  introduction: string;
+  rules: string[];
+  images?: File;
+  joinQuestions: string[];
+}
+
+export interface Club extends Group {
+  meetingPlace: {
+    name: string;
+    placeUrl: string;
+    kakaoMapId: number;
+    addressName: string;
+    roadAddressName: string;
+    latitude: string;
+    longitude: string;
+  };
+}
+
+export interface Meeting extends Group {
+  activityRegion: string;
+}
