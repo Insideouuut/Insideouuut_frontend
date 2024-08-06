64ac7060daeb0997de261961ffea31573566843b
gangmin2 <min0115b@gmail.com>
2024-08-06 12:48:55 +0900

    [#26] 🐛 api content-type 수정

--- src/api/createModongApi.ts
+++ src/api/createModongApi.ts
@@ -1,4 +1,4 @@
-import { Club, Meeting } from '@/types/Modong';
+import { Meeting } from '@/types/Modong';
 import axiosInstance from './axiosConfig';
 
 export const createMeeting = async (
@@ -8,11 +8,20 @@ export const createMeeting = async (
   const response = await axiosInstance.post(
     `api/meetings/${meetingId}`,
     meetingData,
+    {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    },
   );
   return response;
 };
 
-export const createClub = async (clubData: Club) => {
-  const response = await axiosInstance.post('api/clubs', clubData);
+export const createClub = async (clubData: FormData) => {
+  const response = await axiosInstance.post('api/clubs', clubData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
   return response;
 };
