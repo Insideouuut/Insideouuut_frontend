d75a7ee3c5f45da2286322c8bd6d1e4596f9b85c
JooHwanLeeJJang <joohwan7063@gmail.com>
2024-08-12 02:42:49 +0900

    [#46] ✨동아리 관리 페이지

--- src/api/clubApi.ts
+++ src/api/clubApi.ts
@@ -7,7 +7,7 @@ import {
 import { MemberAuthorityApiResponse } from '@/types/MemberAuthorityResponse';
 import axiosInstance from './axiosConfig';
 
-// 동아리 단건 조회
+//동아리 단건 조회
 export const getClubData = async (id: string): Promise<ClubData> => {
   try {
     const response = await axiosInstance.get<ClubApiResponse>(
@@ -190,4 +190,17 @@ export const updateClubData = async (
     console.error('Error updating club data:', error);
     throw error;
   }
+}
+// 동아리 멤버 조회
+export const getClubMemberList = async (clubId: string) => {
+  const response = await axiosInstance.get(`/api/clubs/${clubId}/members`);
+  return response;
+};
+
+// 모임 멤버 조회
+export const getMeetingMemberList = async (meetingId: string) => {
+  const response = await axiosInstance.get(
+    `/api/meetings/${meetingId}/members`,
+  );
+  return response;
 };
