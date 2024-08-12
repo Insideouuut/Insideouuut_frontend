8f52c0685a7bb19ff864d2570d9c6125f9b90e10
gangmin2 <min0115b@gmail.com>
2024-08-12 18:35:28 +0900

    [#47] ✨ access 토큰 만료 시 재발급 구현

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { reissueToken } from './authApi';
 
 const axiosInstance = axios.create({
   baseURL: 'https://modong-backend.site',
@@ -8,13 +9,43 @@ const axiosInstance = axios.create({
 // JWT 토큰을 요청 헤더에 추가하는 인터셉터
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('accessToken'); // 로컬 스토리지에서 토큰 가져오기
+    const token = localStorage.getItem('accessToken') || '';
     if (token) {
       config.headers.Authorization = token;
     }
     return config;
   },
   (error) => Promise.reject(error),
+);
+
+// access 토큰 만료 시 재발급하는 인터셉터
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem('accessToken');
+
+      try {
+        const response = await reissueToken();
+        const statusCode = response.data.status.code;
+        const statusMessage = response.data.status.message;
+        const token = response.headers['authorization'];
+
+        if (statusCode === 200) {
+          localStorage.setItem('accessToken', token);
+          return axiosInstance(originalRequest);
+        } else {
+          alert(statusMessage);
+          window.location.href = '/login';
+        }
+      } catch (error) {
+        console.error('reissueToken:', error);
+        alert('Failed to reissue token: ' + error);
+        window.location.href = '/login';
+      }
+    }
+  },
 );
 
 export default axiosInstance;
